c0a1fabf42c7f81132a4831f78f47dfdc411445d
Alejandro Menocal <amenocal@github.com>
2024-05-17 16:58:40 -0500

    devcontainer

---
+++ .devcontainer/on-create-command.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /source
+cp -r monorepositorio monorepositorio-filter-repo
+cp -r monorepositorio monorepositorio-graft
+cp -r monorepositorio mal-monorepositorio-historial
+mv monorepositorio mal-monorepositorio
